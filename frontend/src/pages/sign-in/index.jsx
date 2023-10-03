2c0e9c5d5a9530fb384c63bb346a68d3d9051895
Jimmy Catalano <jimmy.catalanoperso@gmail.com>
2023-10-03 17:54:01 +0200

    Creating the login page

---
+++ frontend/src/pages/sign-in/index.jsx
@@ -0,0 +1,31 @@
+import { Link } from "react-router-dom";
+import "font-awesome/css/font-awesome.min.css";
+import "./sign-in.css";
+
+export function SignIn() {
+  return (
+    <main className="main bg-dark">
+      <section className="sign-in-content">
+        <i className="fa fa-user-circle  sign-in-icon"></i>
+        <h1>Sign in</h1>
+        <form>
+          <div className="input-wrapper">
+            <label for="username">Username</label>
+            <input type="text" id="username"></input>
+          </div>
+          <div className="input-wrapper">
+            <label for="password">Password</label>
+            <input type="password" id="password"></input>
+          </div>
+          <div class="input-remember">
+            <input type="checkbox" id="remember-me"></input>
+            <label for="remember-me">Remember me</label>
+          </div>
+          <Link className="sign-in-button" to="/user">
+            Sign In
+          </Link>
+        </form>
+      </section>
+    </main>
+  );
+}
